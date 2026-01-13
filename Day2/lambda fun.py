6bdc3dc9154be5dcd230c34bf86e3170d4b61521
omkarnathreddy789-png <omkarnathreddy789@gmail.com>
2026-01-13 16:55:30 +0530

    Add files via upload

---
+++ Day2/lambda fun.py
@@ -0,0 +1,5 @@
+# Define lambda function
+add = lambda a, b: a + b
+
+# Call the lambda function
+print(add(3, 5))
---
+++ Day2/mapping.py
@@ -0,0 +1,8 @@
+# List of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Use map with lambda to double each number
+result = map(lambda x: x * 2, numbers)
+
+# Convert map object to list and print
+print(list(result))
